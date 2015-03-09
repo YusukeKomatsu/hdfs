1a2017f34debda47782c2a53ad5cde3c23c05587
Yusuke Komatsu <tienlen042@gmail.com>
2015-03-09 15:06:12 +0900

    add information

--- hdfs.go
+++ hdfs.go
@@ -7,8 +7,10 @@ import (
 
 func main() {
   app := cli.NewApp()
-  app.Name = "hdfs"
-  app.Usage = "testing yeah!!"
+  app.Name   = "hdfs"
+  app.Usage  = "testing yeah!!"
+  app.Author = "Yusuke Komatsu"
+  app.Email  = "tienlen042@gmail.com"
   app.Action = func(c *cli.Context) {
     println("Hello, human!!\n");
   }
